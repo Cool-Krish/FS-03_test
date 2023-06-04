ac24214f204813b4366467c941d2683ed7af117c
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-06-04 22:14:05 +0530

    :fire: change

---
+++ Flex_Business/flex-business/src/components/Nav-bar.jsx
@@ -0,0 +1,23 @@
+import './style.css'
+
+
+const NavBar = () =>{
+    return <>
+        <div className="header">
+        <div className="Logo">
+        <h1>Flex Business Pro</h1>
+        <p style={{color:"gray"}}>Clean and Modern Business Theme</p>
+        </div>
+        <div className='kk'>
+        
+            <div className="menu">Home</div>
+            <div className="menu">About Us</div>
+            <div className="menu">Styleguide</div>
+            <div className="menu">Blog</div>
+            <div className="menu">Contact Us</div>
+            </div>
+        </div>
+              
+        </>
+}
+export default NavBar;
